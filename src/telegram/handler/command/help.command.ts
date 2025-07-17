a1091a443fbaffd921765e85ed6b7a63164f513b
Jagmesh <justagoodman312@gmail.com>
2025-07-17 23:04:38 +0300

    feat: add setReaction err handling

--- src/telegram/handler/command/help.command.ts
+++ src/telegram/handler/command/help.command.ts
@@ -4,7 +4,7 @@ import {TG_TEXT} from "../../common/text/text.const.js";
 export async function commandHelp(ctx: Context): Promise<void> {
     await Promise.all([
             ctx.sendMessage(TG_TEXT.ENTRY_MESSAGE, {parse_mode: 'HTML'}),
-            ctx.sendMessage(TG_TEXT.INSTUCTIONS_MESSAGE, {parse_mode: 'HTML'}),
+            ctx.sendMessage(TG_TEXT.INSTRUCTIONS_MESSAGE, {parse_mode: 'HTML'}),
             ctx.sendMessage(TG_TEXT.FEEDBACK_MESSAGE, {parse_mode: 'HTML'})
         ])
 }
